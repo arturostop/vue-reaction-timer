6433c61e560f048e046db08ca4c704517f706fdc
arturostop <arturo_parada_95@hotmal.com>
2022-11-10 18:25:43 -0700

    first commit

---
+++ src/components/Block.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">
+    Click me
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['dealy'],
+    data() {
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },    
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = true
+            this.startTimer()
+        }, this.dealy)
+    },
+    methods: {
+        startTimer() {
+            this.timer = setInterval(() => {
+                this.reactionTime += 10
+            },10)
+        },
+        stopTimer() {
+            clearInterval(this.timer)
+            this.$emit('end', this.reactionTime)
+        }
+    }
+}
+</script>
+
+<style>
+    .block {
+        width: 400px;
+        border-radius: 20px;
+        background: #0faf87;
+        color: white;
+        padding: 100px;
+        margin: 40px auto;
+    }
+
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template> 
+    <p>Reaction time - {{ score }} milliseconds</p>
+    <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return{
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250){
+            this.rank = 'Ninja Fingers'
+        } else if (this.score < 400) {
+            this.rank = 'Good reflexes'
+        } else {
+            this.rank = 'Too slow...'
+        }
+    }
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
